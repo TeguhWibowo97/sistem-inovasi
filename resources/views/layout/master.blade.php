7546a748fd39b513e782dcad28d3466834918606
TeguhWibowo97 <teguhwibowo9797@gmail.com>
2020-02-27 10:02:23 +0700

    edit template utama

--- resources/views/layout/master.blade.php
+++ resources/views/layout/master.blade.php
@@ -140,7 +140,7 @@
         <div class="sidebar-brand-icon rotate-n-15">
           <i class="fas fa-laugh-wink"></i>
         </div>
-        <div class="sidebar-brand-text mx-3">Sistem Inovasi</div>
+        <div class="sidebar-brand-text mx-3">MENU</div>
       </a>
 
       <!-- Divider -->
@@ -208,16 +208,7 @@
           </button>
 
           <!-- Topbar Search -->
-          <form class="d-none d-sm-inline-block form-inline mr-auto ml-md-3 my-2 my-md-0 mw-100 navbar-search">
-            <div class="input-group">
-              <input type="text" class="form-control bg-light border-0 small" placeholder="Search for..." aria-label="Search" aria-describedby="basic-addon2">
-              <div class="input-group-append">
-                <button class="btn btn-primary" type="button">
-                  <i class="fas fa-search fa-sm"></i>
-                </button>
-              </div>
-            </div>
-          </form>
+          <h3 class="text-warning">SISTEM PENDATAAN BORANG KINERJA INOVASI UDINUS</h3>
 
           <!-- Topbar Navbar -->
           <ul class="navbar-nav ml-auto">
